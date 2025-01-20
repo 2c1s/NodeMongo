a04a99696443655d27f35083746c2228f3206bed
2c1s <yosef.safi@iths.se>
2025-01-20 15:07:13 +0100

    sista för dagen

--- server.js
+++ server.js
@@ -15,6 +15,36 @@ app.post("/blogpost", (req, res) => {
 res.send("this is a post request")
 });
 
+app.get("/api/product/:id", async(req, res) => {
+    try {
+        const {id} = req.params;
+            const product = await Product.findById(id);
+            res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500);
+    }
+});
+
+app.put("/api/product/:id", async(req, res) => {
+    const {id} = req.params;
+    const prodcut = await Product.findByIdAndUpdate(id, req.body);
+
+    if(!product){
+        return res.status(400).json({message: "Product does not exist"});
+    }
+
+    res.status(200).json(product);
+});
+
+app.get("/api/products", async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 app.post("/api/product", async (req, res) => {
 
