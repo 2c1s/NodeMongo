77393fe4149a8a506057174ff43827763621928f
2c1s <yosef.safi@iths.se>
2025-01-20 11:47:55 +0100

    la till nya mongodb

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const PORT = 3000;
 
 
+app.get("/", (req, res) => {
+    res.send("Welcome to my MongoAPI")
+});
+
+
+app.post("/blogpost", (req, res) => {
+res.send("this is a post request")
+});
 
 
 
-app.listen(PORT, () =>{
-    console.log("API is up and running on Localhost:3000"+ PORT)
+mongoose.
+connect("mongodb+srv://2c1s:Yosef123@yosefsapi.zkkbt.mongodb.net/Products-API?retryWrites=true&w=majority&appName=YosefsAPI")
+.then(() => {
+    console.log("connected to mongo");
+    app.listen(PORT, () =>{
+        console.log("API is up and running on Localhost:"+ PORT)
+    });
 });
