e64c08d8983a7eef8416fd177ca6673f71d0774b
2c1s <yosef.safi@iths.se>
2025-01-20 10:37:52 +0100

    la till fler

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+
+
+
+app.listen(PORT, () =>{
+    console.log("API is up and running on Localhost:3000"+ PORT)
+});
